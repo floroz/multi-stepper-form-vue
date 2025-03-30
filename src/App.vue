4897eaea3b81fccb13e679208311539f3c34c5d5
Daniele Tortora <45793869+floroz@users.noreply.github.com>
2025-03-30 16:07:11 +0200

    chore: create skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MultiStepForm from "./components/MultiStepForm.vue";
+</script>
 
-<template><h1 class="text-9xl">Hello World</h1></template>
+<template>
+  <main
+    class="bg-orange-100 w-full min-h-screen flex flex-col justify-center items-center"
+  >
+    <h1 class="text-3xl mb-4">Join Our Community of Developers</h1>
+    <h2 class="text-md mb-4">
+      To join our community and participate in frontend challenges.<br />
+      Please fill out the following form
+    </h2>
+    <MultiStepForm />
+  </main>
+</template>
 
 <style scoped></style>
