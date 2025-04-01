2c8d7b8b7678303add372d1b985ff64cc1e7bca3
Daniele Tortora <45793869+floroz@users.noreply.github.com>
2025-04-01 11:20:15 +0200

    feat: tech choice

---
+++ src/components/MultiStepForm/MultiFormTechChoice.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+const fullName = defineModel("full-name", { required: true });
+const email = defineModel("email", { required: true });
+const phone = defineModel("phone", { required: true });
+const portfolio = defineModel("portfolio", { required: true });
+</script>
+
+<template>
+  <section class="border-y-1 border-gray-200 p-8">
+    <h3 class="text-lg font-bold">Personal Information</h3>
+    <p class="mb-4 text-sm">
+      Please provide your personal details so we can get to know you better
+    </p>
+
+    <div class="grid grid-cols-2 grid-rows-1 gap-8">
+      <div class="flex flex-col">
+        <label for="" class="text-xs font-bold">Full Name</label>
+        <input
+          placeholder="Rishi Purwar"
+          type="text"
+          class="rounded-md border-1 border-gray-200 p-2 text-black"
+          v-model="fullName"
+        />
+      </div>
+      <div class="flex flex-col">
+        <label for="" class="text-xs font-bold">Email</label>
+        <input
+          placeholder="name@email.com"
+          type="text"
+          class="rounded-md border-1 border-gray-200 p-2 text-black"
+          v-model="email"
+        />
+      </div>
+      <div class="flex flex-col">
+        <label for="" class="text-xs font-bold">Phone</label>
+        <input
+          placeholder="+91 1234567890"
+          type="text"
+          class="rounded-md border-1 border-gray-200 p-2 text-black"
+          v-model="phone"
+        />
+      </div>
+      <div class="flex flex-col">
+        <label for="" class="text-xs font-bold">Portfolio</label>
+        <input
+          placeholder="github.com/rishipurwal"
+          type="text"
+          class="rounded-md border-1 border-gray-200 p-2 text-black"
+          v-model="portfolio"
+        />
+      </div>
+    </div>
+  </section>
+</template>
